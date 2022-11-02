12a6af79a00db4db9aca722d3cafc3feb815af6c
NecroBread <mariovyordanov@gmail.com>
2022-11-02 11:04:00 +0200

    minor cleanup

--- src/app/shared/comments/comments.service.ts
+++ src/app/shared/comments/comments.service.ts
@@ -2,10 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ICommentsServerResponse } from 'src/app/shared/interfaces/ICommentsServerResponse';
 import { IOneCommentServerResponse } from 'src/app/shared/interfaces/IOneCommentServerResponse';
-import { environment } from 'src/environments/environment';
 import { UserService } from '../user/user.service';
-
-const API_URL = environment.apiUrl;
 
 @Injectable({
     providedIn: 'root'
@@ -18,13 +15,13 @@ export class CommentsService {
     constructor(private userService: UserService, private http: HttpClient) { }
 
     getComments(id: string) {
-        return this.http.get<ICommentsServerResponse>(API_URL + `/collections/comments?where=post=${id}&sortBy=createdAt desc&populate=owner`);
+        return this.http.get<ICommentsServerResponse>(`/api/collections/comments?where=post=${id}&sortBy=createdAt desc&populate=owner`);
     }
 
     postComment(body: string, postId: string) {
-        return this.http.post<IOneCommentServerResponse>(API_URL + `/collections/comments`, {
+        return this.http.post<IOneCommentServerResponse>(`/api/collections/comments`, {
             body: body,
             post: postId,
-        }, { withCredentials: true });
+        });
     }
 }
